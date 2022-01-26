91a97b0c4bbaa23665657aed2357bfc2fa5e93f9
Arkadiusz Florczak <arkadiuszflorczak@Arkadiuszs-MacBook-Pro.local>
2022-01-26 22:20:07 +0100

    Add global context

--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom';
 import 'normalize.css';
 import './index.css';
 import App from './App';
-
+import { AppProvider } from '../src/context/appContext';
 
 ReactDOM.render(
-    <React.StrictMode>
-        <App/>
-    </React.StrictMode>,
-    document.getElementById('root')
+  <React.StrictMode>
+    <AppProvider>
+      <App />
+    </AppProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
-
-
