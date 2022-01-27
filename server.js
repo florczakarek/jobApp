9a3fe3710bc395ad2717389ec8b42d65c2135b5c
Arkadiusz Florczak <arkadiuszflorczak@Arkadiuszs-MacBook-Pro.local>
2022-01-27 17:06:51 +0100

    Check setup

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ const start = async () => {
   try {
     await connectDB(process.env.MONGO_URL);
     app.listen(port, () => {
-      console.log(`Server is listening on port ${port}`);
+      console.log(`Server is listening on port ${port}...`);
     });
   } catch (error) {
     console.log(error);
