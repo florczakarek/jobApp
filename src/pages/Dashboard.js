8260c5a59ee3f9562d943c705512fd019c9d057f
Arkadiusz Florczak <arkadiuszflorczak@Arkadiuszs-MacBook-Pro.local>
2022-01-31 18:11:44 +0100

    package.json configured proxy address

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,6 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 const Dashboard = () => {
+  const fetchData = async () => {
+    try {
+      const response = await fetch('/api/v1');
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return <div>Main dashboard</div>;
 };
 
